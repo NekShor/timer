3985153982b2e06f586f2859f7a7a6f169bfb7d7
Antoine Bigot <62606118+NekShor@users.noreply.github.com>
2025-03-03 11:28:15 +0100

    tile

--- public/Js/data.js
+++ public/Js/data.js
@@ -31,20 +31,63 @@ function nekshor_style() {
         jours.classList.add("dateTile");
         date.appendChild(jours);
 
+        var jours_value = document.createElement("div");
+        jours_value.classList.add("jours_value");
+        jours_value.classList.add("dateTileValue");
+        jours.appendChild(jours_value);
+
+        var jours_text = document.createElement("div");
+        jours_text.classList.add("jours_text");
+        jours_text.classList.add("dateTileText");
+        jours_text.innerHTML = "Jours";
+
         var heures = document.createElement("div");
         heures.classList.add("heures");
         heures.classList.add("dateTile");
         date.appendChild(heures);
+
+        var heures_value = document.createElement("div");
+        heures_value.classList.add("heures_value");
+        heures_value.classList.add("dateTileValue");
+        heures.appendChild(heures_value);
+
+        var heures_text = document.createElement("div");
+        heures_text.classList.add("heures_text");
+        heures_text.classList.add("dateTileText");
+        heures_text.innerHTML = "Heures";
+        heures.appendChild(heures_text);
 
         var minutes = document.createElement("div");
         minutes.classList.add("minutes");
         minutes.classList.add("dateTile");
         date.appendChild(minutes);
 
+        var minutes_value = document.createElement("div");
+        minutes_value.classList.add("minutes_value");
+        minutes_value.classList.add("dateTileValue");
+        minutes.appendChild(minutes_value);
+
+        var minutes_text = document.createElement("div");
+        minutes_text.classList.add("minutes_text");
+        minutes_text.classList.add("dateTileText");
+        minutes_text.innerHTML = "Minutes";
+        minutes.appendChild(minutes_text);
+
         var secondes = document.createElement("div");
         secondes.classList.add("secondes");
         secondes.classList.add("dateTile");
         date.appendChild(secondes);
+
+        var secondes_value = document.createElement("div");
+        secondes_value.classList.add("secondes_value");
+        secondes_value.classList.add("dateTileValue");
+        secondes.appendChild(secondes_value);
+
+        var secondes_text = document.createElement("div");
+        secondes_text.classList.add("secondes_text");
+        secondes_text.classList.add("dateTileText");
+        secondes_text.innerHTML = "Secondes";
+        secondes.appendChild(secondes_text);
 
         newElement.appendChild(date);
 
