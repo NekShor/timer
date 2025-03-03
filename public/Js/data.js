3e1c0a96ce4e90ab93361b17bda936f626fe33e3
Antoine Bigot <62606118+NekShor@users.noreply.github.com>
2025-03-03 12:26:02 +0100

    change syntax

--- public/Js/data.js
+++ public/Js/data.js
@@ -100,7 +100,8 @@ function nekshor_style() {
         secondes.appendChild(secondes_text);
 
         var eventDate = new Date(element.date);
-        newElement.setAttribute("onclick", "openlink('https://www.google.com/calendar/render?action=TEMPLATE&text=" + element.name + "&dates=" + eventDate.toISOString().replace(/-|:|\.\d+/g, "").replace(/T/g, "/") + "/" + eventDate.toISOString().replace(/-|:|\.\d+/g, "").replace(/T/g, "/") + "&details=&location=&sf=true&output=xml)'");
+        var url = "https://www.google.com/calendar/render?action=TEMPLATE&text=" + element.name + "&dates=" + eventDate.toISOString().replace(/-|:|\.\d+/g, "").replace(/T/g, "/") + "/" + eventDate.toISOString().replace(/-|:|\.\d+/g, "").replace(/T/g, "/") + "&details=&location=&sf=true&output=xml";
+        newElement.setAttribute("onclick", "openlink('" + url + "')");
 
         newElement.appendChild(date);
 
